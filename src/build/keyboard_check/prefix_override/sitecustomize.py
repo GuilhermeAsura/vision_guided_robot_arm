c12ea90bc24ca0c1d802c40f49c3675b388b76ed
GuilhermeAsura <gmslazzarini@gmail.com>
2025-11-27 18:02:18 -0300

    integrating 'reticencias' packages into the project workspace

--- src/build/keyboard_check/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/trainee/workspace/trainee_ws_pinguin/src/install/keyboard_check'
--- src/build/webots_packed/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/trainee/workspace/trainee_ws_pinguin/src/install/webots_packed'
