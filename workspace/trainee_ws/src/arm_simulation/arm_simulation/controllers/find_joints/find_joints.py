e0edf5930cfa8f552018aacbc73bac331293cadf
Diego de Miranda <diego.demiranda@icloud.com>
2025-11-28 18:58:37 -0300

    Delete workspace/trainee_ws/src/arm_simulation directory

--- workspace/trainee_ws/src/arm_simulation/arm_simulation/controllers/find_joints/find_joints.py
+++
@@ -1,8 +0,0 @@
-from controller import Robot
-
-robot = Robot()
-timestep = int(robot.getBasicTimeStep())
-
-for i in range(robot.getNumberOfDevices()):
-    device = robot.getDeviceByIndex(i)
-    print(device.getName())
